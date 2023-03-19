d8662add250992fa5fe061e39d0b290f4d800ba4
MohammadReza Keikavousi <53637377+keikaavousi@users.noreply.github.com>
2023-03-19 10:31:43 +0100

    feat/add infinite scrolling

--- src/data/moviesSlice.js
+++ src/data/moviesSlice.js
@@ -1,27 +1,34 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const fetchMovies = createAsyncThunk('fetch-movies', async (apiUrl) => {
-    const response = await fetch(apiUrl)
-    return response.json()
-})
+export const fetchMovies = createAsyncThunk("fetch-movies", async (apiUrl) => {
+	const response = await fetch(apiUrl);
+	return response.json();
+});
 
 const moviesSlice = createSlice({
-    name: 'movies',
-    initialState: { 
-        movies: [],
-        fetchStatus: '',
-    },
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(fetchMovies.fulfilled, (state, action) => {
-            state.movies = action.payload
-            state.fetchStatus = 'success'
-        }).addCase(fetchMovies.pending, (state) => {
-            state.fetchStatus = 'loading'
-        }).addCase(fetchMovies.rejected, (state) => {
-            state.fetchStatus = 'error'
-        })
-    }
-})
+	name: "movies",
+	initialState: {
+		movies: [],
+		fetchStatus: "",
+	},
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchMovies.fulfilled, (state, action) => {
+				if (action.payload.page === 1) {
+					state.movies = [action.payload];
+				} else {
+					state.movies = [...state.movies, action.payload];
+				}
+				state.fetchStatus = "success";
+			})
+			.addCase(fetchMovies.pending, (state) => {
+				state.fetchStatus = "loading";
+			})
+			.addCase(fetchMovies.rejected, (state) => {
+				state.fetchStatus = "error";
+			});
+	},
+});
 
-export default moviesSlice
+export default moviesSlice;
